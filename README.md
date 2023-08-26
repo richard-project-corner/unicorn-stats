04fc5643ec60de78f103254457e686212cd01449
richardProjectCorner <r@rboyd.dev>
2023-08-26 20:30:29 +0000

    moving OpenAI stuff to second template

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # unicorn-stats
 Unicorn Stats Service
-
-curl https://w6kfeq70nb.execute-api.us-west-2.amazonaws.com/Prod/.well-known/oas3.yaml
--- unicorn-stats-api/tests/unit/create_login.py
+++ unicorn-stats-api/tests/unit/create_login.py
@@ -5,10 +5,10 @@ import urllib
 import json
 import requests
 
-
 def get_credentials(role_arn: str):
     ACCESS_KEY_ID = os.environ.get("ACCESS_KEY_ID")
     SECRET_ACCESS_KEY = os.environ.get("SECRET_ACCESS_KEY")
+    
     dev = boto3.session.Session(
         aws_access_key_id=ACCESS_KEY_ID,
         aws_secret_access_key=SECRET_ACCESS_KEY
@@ -23,13 +23,14 @@ def get_credentials(role_arn: str):
     return credentials
 
 def main():
+    REGION = os.environ.get("REGION")
     credentials = get_credentials(role_arn='arn:aws:iam::617267062318:role/Console-Role')
     json_credentials = {
         "sessionId": credentials['AccessKeyId'],
         "sessionKey": credentials['SecretAccessKey'],
         "sessionToken": credentials['SessionToken']
     }
-    environment_url=f"https://us-west-2.console.aws.amazon.com"
+    environment_url=f"https://{REGION}.console.aws.amazon.com"
     url_prefix=f"https://signin.aws.amazon.com/federation?Action=getSigninToken&Session={urllib.parse.quote(json.dumps(json_credentials))}"
     signin_token_response = requests.get(url_prefix)
     signin_link = f"""https://signin.aws.amazon.com/federation?Action=login&Destination={urllib.parse.quote(environment_url)}&SigninToken={signin_token_response.json()['SigninToken']}"""
